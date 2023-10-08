a063d5dd93d109f5f37ae4d896146456a867422d
chand <chand@192.168.0.27>
2023-10-08 18:55:11 +0100

    n

---
+++ Package/src/Test.java
@@ -0,0 +1,19 @@
+import java.util.DoubleSummaryStatistics;
+
+public class Test {
+ int salary;
+ int employeedid;
+ 
+ public void method()
+ 
+ {System.out.println("welcome all");
+
+ 
+}
+public	static void main
+	
+	
+	
+	
+	
+}
